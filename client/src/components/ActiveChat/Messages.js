9d8c1ae22318ec1b6d556d35392d0a8123d06bc9
s-munro <sjm.munro@gmail.com>
2021-07-23 22:45:01 -0400

    chore: read message icon added

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
@@ -6,13 +6,18 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const lastUnreadIndex = useMemo(() => messages.findIndex((message) => {
+    return message.senderId === userId && message.read === false;
+  }), [messages, userId]);
+
+
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, idx) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble key={message.id} text={message.text} time={time} otherUser={otherUser} showReadBubble={lastUnreadIndex === idx} />
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );

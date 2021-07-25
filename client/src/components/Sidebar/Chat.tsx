884d270bf1936c9e072f51381d82d1caac41e683
s-munro <sjm.munro@gmail.com>
2021-07-25 17:11:04 -0400

    ref: Chat.js to tsx functional component

---
+++ client/src/components/Sidebar/Chat.tsx
@@ -0,0 +1,47 @@
+import { Box } from '@material-ui/core';
+import { BadgeAvatar, ChatContent } from '.';
+import { makeStyles } from '@material-ui/core/styles';
+import { setActiveChat } from '../../store/activeConversation';
+import { useDispatch } from 'react-redux';
+
+import { Conversation } from '../../conversation-types';
+
+const useStyles = makeStyles(() => ({
+ root: {
+  borderRadius: 8,
+  height: 80,
+  boxShadow: '0 2px 10px 0 rgba(88,133,196,0.05)',
+  marginBottom: 10,
+  display: 'flex',
+  alignItems: 'center',
+  '&:hover': {
+   cursor: 'grab',
+  },
+ },
+}));
+
+type ChatProps = {
+ conversation: Conversation;
+};
+
+export const Chat = ({ conversation }: ChatProps) => {
+ const otherUser = conversation.otherUser;
+ const classes = useStyles();
+ const dispatch = useDispatch();
+
+ const handleClick = () => {
+  dispatch(setActiveChat(otherUser.username));
+ };
+
+ return (
+  <Box onClick={() => handleClick()} className={classes.root}>
+   <BadgeAvatar
+    photoUrl={otherUser.photoUrl}
+    username={otherUser.username}
+    online={otherUser.online}
+    sidebar={true}
+   />
+   <ChatContent conversation={conversation} />
+  </Box>
+ );
+};

79a6f4231d949f28ebf307357aaa842ca087a0bd
s-munro <sjm.munro@gmail.com>
2021-07-24 20:47:25 -0400

    chore: sidebanner and icon

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { ReactComponent as Bubble } from '../assets/icons/bubble.svg';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    primary: {
-      color: 'white',
-      fontSize: 14,
-    }
-  }
-}));
-
 export const BubbleIcon = ({ ...props }) => {
-  const classes = useStyles();
-  return <Bubble className={classes.primary} {...props} />;
+  return <Bubble{...props} />;
 };

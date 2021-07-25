411309df666fe1d68d7846373723087e0fffd6e4
s-munro <sjm.munro@gmail.com>
2021-07-24 20:47:38 -0400

    chore: login completed

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Redirect, useHistory } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
 import {
   Grid,
   Box,
@@ -8,12 +8,68 @@ import {
   Button,
   FormControl,
   TextField,
-} from "@material-ui/core";
-import { login } from "./store/utils/thunkCreators";
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { login } from './store/utils/thunkCreators';
+import { SideBanner } from './components/SideBanner';
+import { AuthForm, HeaderContent } from './components/AuthPage';
+import { theme } from './themes/theme';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    '& .MuiInputLabel-formControl': {
+      color: theme.palette.secondary.main,
+      fontWeight: 600,
+    },
+    flexGrow: 1,
+    height: '100vh',
+  },
+  section: {
+    height: '100%',
+    width: '100%',
+  },
+  mainContent: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: "0px", [theme.breakpoints.down('sm')]: {
+      paddingTop: "150px",
+      paddingBottom: "150px",
+      position: "relative",
+    },
+  },
+  headerContent: {
+    position: 'absolute',
+    top: 0,
+    color: theme.palette.secondary.main,
+  },
+  fullWidth: {
+    width: "100%",
+    paddingTop: "15px",
+    fontWeight: "bold",
+    marginBottom: theme.spacing(4),
+  },
+  label: {
+    fontWeight: 800,
+  },
+  endAdornmentText: {
+    fontWeight: 700,
+    fontSize: "12px",
+    color: theme.palette.primary.main,
+    paddingBottom: theme.spacing(0),
+  },
+  resize: {
+    fontSize: "20px",
+    letterSpacing: 1,
+  }
+}));
 
 const Login = (props) => {
-  const history = useHistory();
   const { user, login } = props;
+
+  const classes = useStyles();
+  const history = useHistory();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -28,40 +84,48 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
+    <Grid className={classes.root} container spacing={0} justifyContent="center" alignItems="center">
+      <Grid container sm={12} md={5} justifyContent="center" className={classes.section}>
+        <SideBanner />
+      </Grid>
+      <Grid container sm={12} md={7} className={classes.mainContent}>
+        <HeaderContent
+          ctaText="Don't have an account?"
+          buttonText="Create account"
+          onButtonClick={() => history.push('/register')}
+          className={classes.headerContent}
+        />
+        <AuthForm
+          headerText="Welcome back!"
+          onSubmit={handleLogin}
+          submitText="Login"
+        >
+          <AuthForm.FormItem
+            aria-label="E-mail address"
+            label="E-mail address"
+            name="username"
+            type="text"
+            className={classes.fullWidth}
+          />
+          <AuthForm.FormItem
+            label="Password"
+            aria-label="password"
+            type="password"
+            name="password"
+            className={`${classes.fullWidth} ${classes.password}`}
+            InputProps={{
+              endAdornment: (
+                <Typography className={classes.endAdornmentText}>
+                  Forgot?
+                </Typography>
+              ),
+              classes: {
+                input: classes.resize,
+              }
+            }}
+          />
+        </AuthForm>
+      </Grid>
     </Grid>
   );
 };
